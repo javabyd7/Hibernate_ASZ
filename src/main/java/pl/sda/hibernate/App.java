d14c7b2a4f71b2c6d42044e2a31653bb6a3e2c15
Artur <szejnaartur@gmail.com>
2019-08-11 10:58:45 +0200

    Nawiązanie połączenia, stworzenia tablicy oraz dodanie pierwszego User-a

--- src/main/java/pl/sda/hibernate/App.java
+++ src/main/java/pl/sda/hibernate/App.java
@@ -1,13 +1,26 @@
 package pl.sda.hibernate;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import pl.sda.hibernate.entity.User;
+
+public class App {
+    public static void main(String[] args) {
+        SessionFactory sf = new Configuration()
+                .configure()
+                .buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        User user = new User("Artur", "Kowalski");
+
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+
+        session.close();
     }
 }
